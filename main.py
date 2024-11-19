bd78a2f6c80d1379603bba7534855c2952cf6320
Tu2-prog <anhtuduong.nguyen@outlook.de>
2024-11-19 23:59:08 +0100

    hot fix

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     # cleaner = info_messages.InfoMessageCleaner()
     # train_df = cleaner.transform_df(train_df)
 
-    # lag_info_extractor = lag_info_extractor.LagInfoExtractor()
-    # train_df = lag_info_extractor.transform(train_df)
+    lag_info_extractor = lag_info_extractor.LagInfoExtractor()
+    train_df = lag_info_extractor.transform(train_df)
 
     # train_type_cf = train_type.TrainTypeClassifier()
     # train_df = train_type_cf.transform_df(train_df)
